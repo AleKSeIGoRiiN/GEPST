bc4577c247ce5d6e5a0be9c31d037fce5e0b44b6
alekseyGorin <lllll.lllll02@mail.ru>
2021-05-22 11:33:06 +0700

    Прощальный коммит

--- app/src/main/java/com/example/gepst/ItemViewModel.kt
+++ app/src/main/java/com/example/gepst/ItemViewModel.kt
@@ -7,11 +7,13 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import java.util.logging.Filter
 
 class ItemViewModel() : ViewModel() {
 
     private val mutableSelectedItem = MutableLiveData<Bitmap>()
     val selectedItem: LiveData<Bitmap> get() = mutableSelectedItem
+    val filters = MutableLiveData<Set<Filter>>()
 
     fun selectItem(bitmap: Bitmap) {
         mutableSelectedItem.value = bitmap
